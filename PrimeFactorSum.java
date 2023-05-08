645e068ecfbc0cb2c037861567b09fe563fb82bc
Abhitej21 <106834809+Abhitej21@users.noreply.github.com>
2023-05-08 15:29:10 +0530

    Update PrimeFactorSum.java

--- PrimeFactorSum.java
+++ PrimeFactorSum.java
@@ -8,6 +8,10 @@ public class Main
         if(n%prime==0)return prime+calculate(n/prime,prime,k,flag);
         if(prime==2)prime=3;
         else{
+		
+	// Every prime number except 2 and 3 can be represented in the form of 6k-1 or 6k+1.
+        /* Hence the next prime numbers can be obtained by the value 6k-1 and 6k+1. It is to be noted that all prime numbers can be represented in form of 6k-1 or 6k+1 
+	 but not all the numbers in form of 6k-1 or 6k+1 are prime number */
             if(flag==false){
                 prime = 6*k-1;
             }
